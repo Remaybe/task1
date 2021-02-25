0efd59575782be7f72bf4de7dc19f0bc07079b97
Maznev Egor <remaybe23@gmail.com>
2021-02-25 16:33:36 +0200

    added second student

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,15 @@ public class Main {
         firstStudent.courseList.add(new Course("JDBC", 24));
         firstStudent.courseList.add(new Course("Spring", 16));
 
-        studentsData.getStats(firstStudent, false);
+        Student secondStudent = new Student("Sidorov Ivan", "AQE",
+                studentsData.inputDate("8 June 2020, Monday, 10:00"));
+        firstStudent.courseList.add(new Course("Test Design", 10));
+        firstStudent.courseList.add(new Course("Page Object", 16));
+        firstStudent.courseList.add(new Course("Selenium", 16));
+
+//        studentsData.getStats(firstStudent, false);
+        studentsData.getStats(secondStudent, true);
+
 
     }
 }
