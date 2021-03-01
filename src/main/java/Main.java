36832362900387f2e94b36f0b6c4600807d790ba
Maznev Egor <remaybe23@gmail.com>
2021-03-02 00:02:41 +0200

    constant removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,24 @@
 public class Main {
 
-    public static final StudentsData STUDENTS_DATA = new StudentsData();
 
     public static void main(String[] args) {
 
+        StudentsData studentsData = new StudentsData();
+
         Student firstStudent = new Student("Ivanov Fedor", "Java Developer",
-                STUDENTS_DATA.setDate("1 June 2020, Monday, 10:00"));
+                studentsData.setDate("1 June 2020, Monday, 10:00"));
         firstStudent.chooseCourse(CourseOrder.JAVA);
         firstStudent.chooseCourse(CourseOrder.JDBC);
         firstStudent.chooseCourse(CourseOrder.SPRING);
 
         Student secondStudent = new Student("Sidorov Ivan", "AQE",
-                STUDENTS_DATA.setDate("8 June 2020, Monday, 10:00"));
+                studentsData.setDate("8 June 2020, Monday, 10:00"));
         secondStudent.chooseCourse(CourseOrder.TEST_DESIGN);
         secondStudent.chooseCourse(CourseOrder.PAGE_OBJECT);
         secondStudent.chooseCourse(CourseOrder.SELENIUM);
 
-        STUDENTS_DATA.getStats(firstStudent, true);
-        STUDENTS_DATA.getStats(secondStudent, true);
+        studentsData.getStats(firstStudent, true);
+        studentsData.getStats(secondStudent, true);
 
 
 
